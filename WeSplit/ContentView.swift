6793c12a2b2429c2f9793efa872388935a75856c
Maria Sandu <mariafilimon@gmail.com>
2023-09-01 12:14:29 +0300

    Initial commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,18 +13,18 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
-
+    
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
-
+        
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
-
+        
         return amountPerPerson
     }
     
@@ -32,7 +32,7 @@ struct ContentView: View {
         let tipSelection = Double(tipPercentage)
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
-
+        
         return grandTotal
     }
     
@@ -43,7 +43,7 @@ struct ContentView: View {
                     TextField("Amount", value: $checkAmount, format: .currency(code:  "USD"))
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
-
+                    
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2 ..< 10) {
                             Text("\($0) people")
@@ -61,16 +61,16 @@ struct ContentView: View {
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
-// The same as above but without segmented style
-//                Section {
-//                    Picker("Tip percentage", selection: $tipPercentage) {
-//                        ForEach(0..<101) {
-//                            Text($0, format: .percent)
-//                        }
-//                    }
-//                } header: {
-//                    Text("How much tip do you want to leave?")
-//                }
+                // The same as above but without segmented style
+                //                Section {
+                //                    Picker("Tip percentage", selection: $tipPercentage) {
+                //                        ForEach(0..<101) {
+                //                            Text($0, format: .percent)
+                //                        }
+                //                    }
+                //                } header: {
+                //                    Text("How much tip do you want to leave?")
+                //                }
                 
                 Section {
                     Text(totalPerCheck, format: .currency(code: "USD") )
@@ -78,7 +78,7 @@ struct ContentView: View {
                 } header: {
                     Text("Total amount for the check")
                 }
-
+                
                 Section {
                     Text(totalPerPerson, format: .currency(code: "USD"))
                 } header: {
@@ -89,13 +89,13 @@ struct ContentView: View {
             .toolbar {
                 ToolbarItemGroup(placement: .keyboard) {
                     Spacer()
-
+                    
                     Button("Done") {
                         amountIsFocused = false
                     }
                 }
             }
-
+            
         }
     }
 }
